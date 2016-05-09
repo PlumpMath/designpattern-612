ecd5505bc8ae8f4af9cb3ca6a3ae0366f5276619
Kimi-Qu <eyres.jones@gmail.com>
2016-05-09 11:01:14 +0800

    modify the code

--- src/main/java/com/sun/demo/showcase/HelloWorldController.java
+++ src/main/java/com/sun/demo/showcase/HelloWorldController.java
@@ -1,10 +1,14 @@
 package com.sun.demo.showcase;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,15 +17,24 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 
 @Controller
-@RequestMapping("/hello")
 public class HelloWorldController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(name = "/hello", method = RequestMethod.GET)
     public
     @ResponseBody
     Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/hello").allowedOrigins("http://localhost:8828");
+            }
+        };
     }
 }
